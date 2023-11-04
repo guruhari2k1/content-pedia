74167d8f9efb8bfbb85a6ea52bb1abdecb5479eb
guruhari2k1 <guruharivenkat2001@gmail.com>
2023-11-04 09:56:17 +0530

    Initializing repository

---
+++ client/src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center w-full h-[100dvh] flex-col gap-8">
+      <h1 className="text-5xl font-bold">Content Overflow</h1>
+      <span className="text-sm font-bold uppercase">work under progress</span>
+    </div>
+  );
+}
+
+export default App;
